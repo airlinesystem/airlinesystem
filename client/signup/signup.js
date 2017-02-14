681d8190bf86a60b7907e4fb0a20342d5cbd7fcf
mohamed <toot2009@hotmail.com>
2017-02-14 19:06:32 +0200

    many changing

--- client/signup/signup.js
+++ client/signup/signup.js
@@ -4,10 +4,11 @@ angular.module('MyApp.signup', ['ngRoute'])
 
 .controller('signupCT', function($scope, account, $location) {
   $scope.data = {};
+  console.log('oiouytfdhi')
   $scope.signup = function(){
     account.signup($scope.data).then(function(data){
      console.log(data)
-    
+    $location.path('/offer.html')
   })
 
   }
