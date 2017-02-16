f82f3090a64a000216ef195d32efaf75004ff6d5
mohamed <toot2009@hotmail.com>
2017-02-17 00:45:26 +0200

    another try whith db

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ var app = express();
 //////*****/////
 // var mongoURI = '';
 // mongoose.connect(mongoURI);
-mongoose.connect('mongodb://root:123@ds161038.mlab.com:61038/mymongodb');
+mongoose.connect('mongodb://users:123@ds161038.mlab.com:61038/mymongodb');
 
 //mongoose.connect('mongodb://localhost/airline')
 app.use(bodyParser.urlencoded({extended: true}));
